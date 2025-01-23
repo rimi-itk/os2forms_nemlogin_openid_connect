c5ae31382c044b7dd29c73c3c86ad5418b5d755e
jekuaitk <jekua@aarhus.dk>
2025-01-23 19:42:28 +0100

    Clean up

--- src/Controller/OpenIDConnectController.php
+++ src/Controller/OpenIDConnectController.php
@@ -170,7 +170,6 @@ class OpenIDConnectController implements ContainerInjectionInterface {
       }
     }
 
-
     $providerOptions = [
       'redirectUri' => $this->getRedirectUri(),
       'openIDConnectMetadataUrl' => $discoveryUrl,
